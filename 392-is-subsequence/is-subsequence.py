f281568d11682e5e188157b9155a380d369a0712
Krishna jha <ikrishnajha21@gmail.com>
2026-09-22 18:48:29 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (66.50%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,5 +1,4 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         it = iter(t)
-        return all(char in it for char in s)
-        
+        return all (char in it for char in s)
